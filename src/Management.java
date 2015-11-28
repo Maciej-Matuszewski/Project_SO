21b53cbc9a5247cce8d5407c61daa282dd5c3e69
PiotrMaleszczuk <maleszczuk.piotr@gmail.com>
2015-11-28 16:54:18 +0100

    Proces Management Init

---
+++ src/Management.java
@@ -0,0 +1,37 @@
+
+public class Management 
+{	
+	static void fork()
+	{
+		Proces proces = new Proces();
+		System.out.println("Powsta³: " + proces.nazwa);
+		
+	}
+	
+	static void fork(Proces proces)
+	{
+		Proces proces2 = new Proces(proces);
+		System.out.println("Powsta³: " + proces.nazwa);
+		
+	}
+	
+	static void kill()
+	{
+		System.out.println("Tu bêdzie metoda kill");
+	}
+	static void exit()
+	{
+		System.out.println("Tu bêdzie metoda exit");
+	}
+	
+	static void exec()
+	{
+		System.out.println("Tu bêdzie metoda exec");
+	}
+	
+	static void ProcessPrint()
+	{
+		System.out.println("Tu bêdzie metoda ProcessPrint");
+	}
+	
+}
---
+++ src/Proces.java
@@ -0,0 +1,28 @@
+
+public class Proces {
+
+	public static int nr = 0;
+	String nazwa;
+	int ID;
+	int IDR;
+	
+	
+
+	
+	
+	Proces()
+	{
+		nr++;
+		nazwa = "proces " + nr;
+		ID = nr;
+	}
+	
+	Proces(Proces proces)
+	{
+		nr++;
+		nazwa = "proces potomny od: " + proces.nazwa;
+		ID = nr;
+		IDR = proces.ID;
+	}
+	
+}
