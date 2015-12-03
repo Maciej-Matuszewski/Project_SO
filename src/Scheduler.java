d89753b747c4e34da281ca7203f9fbe04f88188e
PBurdelak <pburdelak@gmail.com>
2015-12-03 11:30:11 +0100

    damy rade panowie

---
+++ src/Scheduler.java
@@ -0,0 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+public class Scheduler {
+	List[] qs = new List[8]; //tablica kolejek
+	boolean[] whichqs = new boolean[8]; //ktore kolejki sa nie puste
+}
