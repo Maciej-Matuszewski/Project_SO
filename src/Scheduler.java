50dba0c7c47f06c3e84ca9ede84fe753810f12ea
PBurdelak <pburdelak@gmail.com>
2015-12-03 22:21:51 +0100

    Commit next

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -2,27 +2,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-	public class Scheduler {
+public class Scheduler {
 		
 	ArrayList<ArrayList<Proces>> qs = new ArrayList<ArrayList<Proces>>(8);
+	ArrayList<Proces> wait_list = new ArrayList<Proces>();
 	boolean[] whichqs = new boolean[8]; //ktore kolejki sa nie puste
 	public int base = 8;
 	
 	Scheduler()
 	{
-		
 		for(int i=0;i<7;i++)
+		{
+			qs.add(i,new ArrayList());
 			whichqs[i] = false;
+		}
 	}
 	
 	public void add_to_ready(Proces pr)
 	{
-		qs.add(0,new ArrayList<Proces>());
 		qs.get(pr.pri/4).add(pr);	
 		whichqs[pr.pri/4] = true;
-
-
-}
+	}
+	
+	public void add_to_wait(Proces pr)
+	{
+		int tmp = qs.get(pr.pri/4).indexOf(pr);
+		wait_list.add(qs.get(pr.pri/4).get(tmp));
+		qs.get(pr.pri/4).remove(tmp);
+	}
 	
 	
 	public boolean przelicz()  
@@ -37,9 +44,9 @@ import java.util.List;
 				{
 					qs.get(i).get(j).cpu /= 2;
 					qs.get(i).get(j).uspri = base + qs.get(i).get(j).cpu/2 + qs.get(i).get(j).nice;
-					
+					qs.get(i).get(j).pri = qs.get(i).get(j).uspri;
 				}
-						//qs[i];
+						
 			}
 		}
 		
