0a083250e0fdb28aabd89ef51dc83f96e84f1472
Florek95 <lukasz.florczak95@gmail.com>
2015-12-06 00:21:08 +0100

    Update and rename FlorekFileSystem to FlorekFileSystem.java

--- src/FlorekFileSystem.java
+++ src/FlorekFileSystem.java
@@ -4,10 +4,21 @@
 /*                  Last update: 28.11.2015 10:37                 */
 /******************************************************************/
 package florekfilesystem;
+import java.util.Scanner;
 /******************************************************************/
 public class FlorekFileSystem {   
-    public static void main(String[] args) {
-       Disk SysDisk = new Disk("Dysk Systemowy", "Florek File System", 1024, 32);  
+    public static void main(String[] args) { 
+       Scanner Command = new Scanner(System.in);
+       String Decision;
+       Disk SysDisk = new Disk("Dysk Systemowy", "Florek File System", 1024, 32); 
+       
+       while(true) {
+           System.out.print("Komenda: ");
+           Decision = Command.nextLine(); 
+           if(Decision.equals("ext"))
+               break;
+           SysDisk.Disk_Command(Decision);
+       }       
     }  
 }
 /******************************************************************/
