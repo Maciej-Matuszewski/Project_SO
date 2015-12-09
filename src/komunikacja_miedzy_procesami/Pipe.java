3559625cd4a9601dfcb6555f401728a1525bfcf4
Gr3mlin106 <maciej.matuszewski@me.com>
2015-12-09 20:41:19 +0100

    komunikacja log to output update

--- src/komunikacja_miedzy_procesami/Pipe.java
+++ src/komunikacja_miedzy_procesami/Pipe.java
@@ -2,6 +2,7 @@ package komunikacja_miedzy_procesami;
 
 import java.util.ArrayList;
 
+import Interpreter.Output;
 import zarzadzanie_procesami.Proces;
 
 public class Pipe {
@@ -18,22 +19,22 @@ public class Pipe {
 			this.parent = parentProces;
 			parentProces.pipes.add(this);
 			childProces.childPipe = new PipeField(this);
-			System.out.println("Utorzono pipe pomiedzy procesem rodzica: "+parent.PID+" a procesem potomka: "+child.PID);
+			Output.write("Utorzono pipe pomiedzy procesem rodzica: "+parent.PID+" a procesem potomka: "+child.PID);
 		}
 	}
 	
 	public void write(String data){
 		this.pipeListToChild.add(data);
 
-		System.out.println("Proces rodzica: "+parent.PID+" zapisał dane dla procesu potomka: "+child.PID+" o treści: \""+data+"\"");
+		Output.write("Proces rodzica: "+parent.PID+" zapisał dane dla procesu potomka: "+child.PID+" o treści: \""+data+"\"");
 	}
 	
 	public String read() throws Exception{
 		if(this.pipeListToParrent.size()>0){
 			String temp = this.pipeListToParrent.get(0);
-			System.out.println("Proces rodzica: "+parent.PID+" odczytał dane od procesu potomka: "+child.PID+" o treści: \""+temp+"\"");
+			Output.write("Proces rodzica: "+parent.PID+" odczytał dane od procesu potomka: "+child.PID+" o treści: \""+temp+"\"");
 			this.pipeListToParrent.remove(0);
-			System.out.println("Dane przeznaczone dla procesu rodzica: "+parent.PID+" o treści: \""+temp+"\" zostały usunięte z pipe");
+			Output.write("Dane przeznaczone dla procesu rodzica: "+parent.PID+" o treści: \""+temp+"\" zostały usunięte z pipe");
 			return temp;
 		}
 		throw new Exception("Kolejka jest pusta");
@@ -44,7 +45,7 @@ public class Pipe {
 		for(int i = 0; i < parent.pipes.size();i++){
 			if(parent.pipes.get(i).equals(this)){
 				parent.pipes.remove(i);
-				System.out.println("Zamknięto pipe pomiedzy procesem rodzica: "+parent.PID+" a procesem potomka: "+child.PID);
+				Output.write("Zamknięto pipe pomiedzy procesem rodzica: "+parent.PID+" a procesem potomka: "+child.PID);
 				break;
 			}
 		}
